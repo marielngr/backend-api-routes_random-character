85518e96c4c04960d1036e770f69d541a231e707
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-02-05 21:54:05 +0100

    solved

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error, isLoading } = useSWR("/api/random-character", fetcher);
+
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
+  if (error) {
+    return <p>error</p>;
+  }
+
+  return (
+    <h1>
+      Hello {data.firstName} {data.lastName}!
+    </h1>
+  );
 }
